3f5aa10023a9ea22ef53e2da464023be17404f62
Leonid Beder <leonid.beder@gmail.com>
2020-12-05 14:17:56 +0200

    Update solcover

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,6 @@
 const memdown = require('memdown');
 
 module.exports = {
-    skipFiles: ['contracts/helpers'],
     providerOptions: {
         db: memdown(),
         default_balance_ether: 10000000000000000000
